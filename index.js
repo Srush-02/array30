7609359a2dccb999e7435ad2c5f29c008fad06c8
Srush-02 <62326610+Srush-02@users.noreply.github.com>
2025-05-05 00:51:35 +0100

    check

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 alert("Hello ex 4")
+let f4 =(a, b, l) => {
+    let sum = 0;
+    for(let i = 0; i < l.length; i++){
+        if (l[i] % a === 0 || l[i] % b === 0) {
+            sum += l[i];
+        }
+    }
+    return sum;
+        
+};
