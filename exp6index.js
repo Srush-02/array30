47dcad1ece109b9d2e19aeaf9725c2c639326648
Srush-02 <62326610+Srush-02@users.noreply.github.com>
2025-05-05 21:08:56 +0100

    check6a

--- exp6index.js
+++ exp6index.js
@@ -1,1 +1,13 @@
 alert("Hello exp 6");
+
+let f6 = (a, l) => {
+    let sum = 0;
+    for (let i = 0; i < l.length; i++) {
+      for (let j = 0; j < a.length; j++) {
+        if (l[i] % a[j] === 0) {
+          sum += l[i];
+        }
+      }
+    }
+    return sum;
+  };
