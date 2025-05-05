9a1b56c26871cd015945dec1908c756e7d202c9e
Srush-02 <62326610+Srush-02@users.noreply.github.com>
2025-05-05 20:23:41 +0100

    checks

--- ex5index.js
+++ ex5index.js
@@ -1,5 +1,6 @@
 alert("Hello Exp 5")
 let f5 = (a, l) => {
+    console.log("enter");
     let sum = 0;
     for (let i = 0; i < l.length; i++) {
         if (l[i] % a[0] === 0 || l[i] % a[1] === 0) {
