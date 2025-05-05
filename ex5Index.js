4dd4786b43bb39087c8c0f005b29a4bbd070cfef
Srush-02 <62326610+Srush-02@users.noreply.github.com>
2025-05-05 19:44:46 +0100

    ex5

---
+++ ex5Index.js
@@ -0,0 +1,10 @@
+alert("Hello Exp 5")
+let f5 = (a, l) => {
+    let sum = 0;
+    for (let i = 0; i < l.length; i++) {
+        if (l[i] % a[0] === 0 || l[i] % a[1] === 0) {
+            sum += l[i];
+        }
+    }
+    return sum;
+};
